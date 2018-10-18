0746a920b4851850a7402d3e591956d479394e5f
m7n9k <m7n9k@unb.ca>
2018-10-18 17:17:41 -0300

    Added footer test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
 import Bpm from '@/components/Bpm.vue';
+import Foot from '@/components/Foot.vue';
+import middleware from '@/middleware';
+
+middleware();
 
 describe('Bpm.vue', () => {
   it('renders props.msg when passed', () => {
@@ -8,5 +12,13 @@ describe('Bpm.vue', () => {
     });
     wrapper.find('.text');
     expect(wrapper.text()).toBe('128 bpm');
+  });
+});
+
+describe('Foot.vue', () => {
+  it('renders with copyright when passed', () => {
+    const wrapper = shallowMount(Foot);
+    wrapper.find('.position');
+    expect(wrapper.text()).toContain('Vuesic');
   });
 });
