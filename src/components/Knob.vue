850690f8e263fa3031bb76c08ec720c116ab1ed0
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-12-01 00:13:53 -0400

    Almost complete Knob refactors. Just need to finish stroke-offset issues

--- src/components/Knob.vue
+++ src/components/Knob.vue
@@ -4,22 +4,27 @@
       class="rela-block knob-dial" 
       :style="knobStyle"
     >
-      <div 
-        class="abs-center dial-grip" 
-        :style="rectStyle" 
-      ></div>
       <svg class="dial-svg" viewBox="0 0 100 100">
         <path
-          d="M20,76 A 40 40 0 1 1 80 76" 
+          :d="rangePath"
           fill="none"
           stroke="#55595C"
+          :stroke-width="strokeWidth"
         ></path>
         <path 
-          d="M20,76 A 40 40 0 1 1 80 76" 
+          :d="rangePath" 
           fill="none" 
-          :stroke="primaryColor" 
+          :stroke="primaryColor"
+          :stroke-width="strokeWidth"
           :style="strokeStyle"
         ></path>
+        <rect
+          :width="rectWidth"
+          :x="center - rectWidth / 2"
+          :height="rectHeight"
+          :fill="primaryColor"
+          :transform="transform"
+        ></rect>
       </svg>
     </div>
     <div
@@ -36,49 +41,93 @@
 import { Draggable } from '@/mixins';
 import { Component, Prop, Mixins, Watch } from 'vue-property-decorator';
 
-// Credit goes to this codepen: https://codepen.io/mavrK/pen/erQPvP
+// TODO These should be computed
+
+// Credit to the styling goes to this codepen: https://codepen.io/mavrK/pen/erQPvP
 // They actually have some nice dials we may want to use
 
 @Component
 export default class Knob extends Mixins(Draggable) {
   @Prop({ type: Number }) public value!: number;
+  @Prop({ type: Number, default: 264 }) public range!: number;
   @Prop({ type: Number, default: 100 }) public max!: number;
   @Prop({ type: Number, default: 0 }) public min!: number;
   @Prop({ type: Number, default: 1 }) public stepSize!: number;
   @Prop({ type: Number, default: 100 }) public size!: number;
   @Prop({ type: String, default: '#409eff' }) public primaryColor!: string;
   @Prop(String) public label?: string;
-  @Prop({ type: Number, default: 17 }) public strokeWidth!: number;
+  @Prop({ type: Number, default: 2.5 }) public strokeWidth!: number;
 
-  public rotation!: number;
-  public knob = {
-    rotation: -132,
-    selected: false,
-  };
+  public rotation = -this.range / 2;
+  public rectWidth = 3;
+  public rectHeight = 25;
 
-  public get strokeDashoffset() {
-    return 184 - (184 / 264) * (this.knob.rotation + 132);
+  get minDegrees() {
+    return 90 + this.angle;
+  }
+  get maxDegrees() {
+    return 90 - this.angle;
+  }
+  get maxRadians() {
+    return this.maxDegrees / 360 * 2 * Math.PI;
+  }
+  get minRadians() {
+    return this.minDegrees / 360 * 2 * Math.PI;
+  }
+  get minX() {
+    return this.center + (Math.cos(this.minRadians) * this.radius);
+  }
+  get minY() {
+    return this.center - (Math.sin(this.minRadians) * this.radius);
+  }
+  get maxX() {
+    return this.center + (Math.cos(this.maxRadians) * this.radius);
+  }
+  get maxY() {
+    return this.center - (Math.sin(this.maxRadians) * this.radius);
   }
 
+  get angle() {
+    return this.range / 2;
+  }
+  get transform() {
+    return `rotate(${this.rotation} ${this.center} ${this.center})`;
+  }
+  get rangePath() {
+    return `M ${this.minX} ${this.minY} A ${this.radius} ${this.radius} 0 1 1 ${this.maxX} ${this.maxY}`;
+  }
+  public get strokeDashoffset() {
+    return 184 - (184 / 264) * (this.rotation + this.angle);
+  }
   public get strokeStyle() {
     return { 'stroke-dashoffset': this.strokeDashoffset };
   }
   public get knobStyle() {
-    return { color: this.primaryColor };
+    return {
+      color: this.primaryColor,
+      height: `${this.size}px`,
+      width: `${this.size}px`,
+    };
   }
-  public get rectStyle() {
-    return { transform: `translate(-50%,-50%) rotate(${this.knob.rotation}deg)` };
+  public get center() {
+    return this.size / 2;
+  }
+  public get radius() {
+    // TODO add comment here
+    return this.center - this.strokeWidth / 2;
   }
 
   public move(e: MouseEvent, { changeY }: { changeY: number }) {
-    this.knob.rotation -= changeY * 1.5; // multiply by a factor to get better speed
-    this.knob.rotation = Math.max(-132, Math.min(132, this.knob.rotation));
-    const value = this.mapRange(this.knob.rotation, -132, 132, this.min, this.max);
+    // Multiply by a factor to get better speed.
+    // This factor should eventually be computed by the changeX
+    this.rotation -= changeY * 1.5;
+    this.rotation = Math.max(-132, Math.min(this.angle, this.rotation));
+    const value = this.mapRange(this.rotation, -this.angle, this.angle, this.min, this.max);
     this.$emit('input', value);
   }
 
-  public mounted() {
-    this.rotation = this.mapRange(this.value, this.min, this.max, -132, 132);
+  public beforeMount() {
+    this.rotation = this.mapRange(this.value, this.min, this.max, -this.angle, this.angle);
   }
 }
 </script>
@@ -95,28 +144,12 @@ export default class Knob extends Mixins(Draggable) {
   display: inline-block
   position: relative
 
-.knob-dial
-  height: 100px
-  width: 100px
-
 .dial-grip
   border-radius: 100%
   z-index: 5
   height: 82px
   width: 82px
 
-  &::after
-    content: ""
-    position: absolute
-    top: 0
-    left: 50%
-    width: 2px
-    transform: translateX(-50%)
-    height: 25px
-    width: 3px
-    border-radius: 4px
-    background-color: currentColor
-    
 .dial-svg
   stroke-width: 2.5
   pointer-events: none
