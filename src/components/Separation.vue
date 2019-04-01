dc0f0dcb7a4639abe9c4f1e5f5fdbea15a09d5c6
Amir Eldesoky <aeldesok@unb.ca>
2019-03-31 23:01:25 -0300

    models are integrated in vusic and tested

--- src/components/Separation.vue
+++ src/components/Separation.vue
@@ -5,14 +5,26 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { workspace } from '@/store';
+import {PythonShell, Options} from 'python-shell';
 
 @Component
 export default class Separation extends Vue {
   @Prop({type: String}) public samplePath!: string;
 
-  public click() {
+public click() {
     if (this.samplePath) {
-      console.log(workspace.pythonPath);
+
+      const options: Options = {
+        mode: 'text',
+        pythonPath: workspace.pythonPath,
+        scriptPath: workspace.modelsPath,
+        args: [this.samplePath],
+      };
+
+      PythonShell.run('vvusic/separation/scripts/separate.py', options, (err?: Error) => {
+        if (err) { throw err; }
+        console.log('success');
+      });
     }
   }
 }
