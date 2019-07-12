2729b46b85b303d24d3541bf1b33863b4ec35ad6
Jacob Smith <jacob.smith@eigeninnovations.com>
2019-07-12 08:33:15 +0200

    removing vuex completly

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import middleware from '@/middleware';
 import App from '@/App.vue';
-import store from '@/store';
 
 middleware();
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
   render: (h) => h(App),
 }).$mount('#app');
--- src/store/index.ts
+++
@@ -1,4 +0,0 @@
-import store from '@/store/store';
-
-export { Project } from '@/store/project';
-export default store;
