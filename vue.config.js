647d335c6f47e8224a31fb2b4382253d2ef9d7ff
Jacob Smith <jacob.smith@unb.ca>
2019-12-03 18:28:07 +0000

    upgrading dependencies

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const config = {
   runtimeCompiler: true,
   pluginOptions: {
     karma: {
+      // FIXME this may not work with the new karma plugin
+      // See https://github.com/tushararora/vue-cli-plugin-unit-karmajs/issues/1
       // Include spec files in src/
       files: [
         'tests/**/*.spec.js',
